e83c02958e0681d8ab90bf9b5d56be4987e895d3
Rakesh1github <atmrakesh143@gmail.com>
2025-11-09 15:47:34 +0530

    Hero Section Inserted

---
+++ src/card/heroSec.jsx
@@ -0,0 +1,135 @@
+   <!--hero section-->
+    <div class="carousel">
+        <!-- list item -->
+        <div class="list">
+            <div class="item">
+                <img src="img/album-designers.jpg">
+                <div class="content">
+                    <div class="author">Photography By</div>
+                    <div class="title">Apshra Studio</div>
+                    <div class="topic">ALBUM DESIGNS</div>
+                    <div class="des">
+                        <!-- lorem 50 -->We offer you the variety of album designs & also cancavera Albums. <br>
+                        professional Wedding Album designers will make unique Wedding Albums.
+                    </div>    
+                </div>
+            </div>
+            <div class="item">
+                <img src="img/pre2.jpg">
+                <div class="content">
+                    <div class="author">Photography By</div>
+                    <div class="title">Apshra Studio</div>
+                    <div class="topic">PRE-WEDDING SHOOT</div>
+                    <div class="des">
+                        The main aim of pre-wedding shoot is to get used to being in front of the camera,
+                        <br> build rapport with your Photographer,& receive a lovely set of romentic photos.
+                    </div>      
+                </div>
+            </div>
+
+            <div class="item">
+                <img src="img/">
+                <div class="content">
+                    <div class="author">Photography By</div>
+                    <div class="title">Apshra Studio</div>
+                    <div class="topic">BABY PHOTOSHOOT</div>
+                    <div class="des">
+                        Contact us today for best Baby photography. <br> 
+                        Our professional Portfolio <br>
+                        Photographers will capture best Model Photoshoot
+                    </div>
+                </div>
+            </div>
+
+            <div class="item">
+                <img src="img/">
+                <div class="content">
+                    <div class="author">Photography By RAKESH</div>
+                    <div class="title">Apshra Studio</div>
+                    <div class="topic">E-COMM. PRODUCT PHOTOSHOOT</div>
+                    <div class="des">
+                        We provide best E-Commerce Photography and professional Products photography in Jharkhand. <br> Contact Us Today!
+                    </div>  
+                </div>
+            </div>
+            <div class="item">
+                <img src="img/">
+                <div class="content">
+                    <div class="author">Photography By</div>
+                    <div class="title">APSHRA STUDIO</div>
+                    <div class="topic">RAKESH</div>
+                    <div class="des">
+                        The mission of Apshra Photographers is just not to snap but to create a beautiful story filled
+                        with joy and emotions that creates lasting memories with passion and excitement, and every 
+                        guest in a family like manner.
+                    </div>  
+                </div>
+            </div>
+        </div>
+        <!-- list thumnail -->
+        <div class="thumbnail">
+            <div class="item">
+                <img src="img/album-designers.jpg">
+                <div class="content">
+                    <div class="title">
+                        ALBUM DESIGNS
+                    </div>
+                    <div class="description">
+                        Description
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <img src="img/pre2.jpg">
+                <div class="content">
+                    <div class="title">
+                        PRE-WEDDING SHOOT
+                    </div>
+                    <div class="description">
+                        Description
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <img src="img/Baby-photoshoot-locations-in-Bangalore.jpg">
+                <div class="content">
+                    <div class="title">
+                        BABY PHOTOSHOOT
+                    </div>
+                    <div class="description">
+                        Description
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <img src="img/">
+                <div class="content">
+                    <div class="title">
+                        E-Comm. PRODUCT PHOTOSHOOT
+                    </div>
+                    <div class="description">
+                        Description
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <img src="img/cameraH.jpg">
+                <div class="content">
+                    <div class="title">
+                        OWNER DESCRIPTION
+                    </div>
+                    <div class="description">
+                        Description
+                    </div>
+                </div>
+            </div>
+        </div>
+       </div> <!-- next prev -->
+
+        <div class="arrows">
+            <button id="prev"><</button>
+            <button id="next">></button>
+        </div>
+        
+        <div class="time"></div>
+    </div>
